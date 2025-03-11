4a60e584f91134fd74e294b69e04ccf2f5b807c6
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-03-11 10:13:36 +0000

    test

---
+++ app/src/main/java/com/cunninghamharry/loginactivity/HomePage.kt
@@ -0,0 +1,4 @@
+package com.cunninghamharry.loginactivity
+
+class HomePage {
+}
---
+++ app/src/main/java/com/cunninghamharry/loginactivity/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.cunninghamharry.loginactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener{
+           val username = usernameInput.text.toString()
+           val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username: $username and Password: $password")
+
+        }
+    }
+
+    fun goToActivity2(view: View?) {
+        val intent = Intent(this, HomePage::class.java)
+        startActivity(intent)
+    }
+}
